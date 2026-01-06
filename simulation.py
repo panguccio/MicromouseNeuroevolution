a6895ffd2d81e0f0e6394ec09224dff6b2c075d4
anna <anna.guccione@studenti.units.it>
2026-01-06 20:04:28 +0100

    added possibility to move with keys for debug

--- simulation.py
+++ simulation.py
@@ -7,21 +7,17 @@ import graphics
 from maze_loader import MazeLoader
 from mouse import Mouse
 
-"""
-This script takes care of the graphical rendering.
-"""
-
 BESTEST_PATH = os.path.join("nets", "bestest_mouse.pkl")
 LATEST_PATH = os.path.join("nets", "latest_mouse.pkl")
 
 best_simulation = False
-
+user_controlled = False
 loader = MazeLoader()
 
 def load_best_mouse():
     path = BESTEST_PATH
     if not os.path.exists(path):
-        raise FileNotFoundError(f"Non trovo il file {path}")
+        raise FileNotFoundError(f"Non trovo il file {path}, ne creo uno nuovo.")
     with open(path, "rb") as f:
         mouse = pickle.load(f)
 
@@ -33,24 +29,41 @@ def move_with_network(maze, mouse):
     outputs = mouse.net.activate(inputs)
     action = outputs.index(max(outputs))
     mouse.act(action, maze)
+    mouse.update_maze_score(maze, 0)
+
+def move_with_keys(event, maze, mouse):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            mouse.act(1, maze)
+        if event.key == pygame.K_LEFT:
+            mouse.act(0, maze)
+        if event.key == pygame.K_RIGHT:
+            mouse.act(2, maze)
 
 def run(sim_mouse=None, maze=None, config=None):
-    global best_simulation
+    global best_simulation, user_controlled
 
     if maze is None:
         maze = loader.get_random_maze()
 
     if sim_mouse is None or sim_mouse.genome is None:
-        sim_mouse = load_best_mouse()
+        try:
+            sim_mouse = load_best_mouse()
+        except FileNotFoundError:
+            sim_mouse = Mouse()
+            user_controlled = True
 
     mouse = Mouse(start_position=maze.start_cell,
                   generation=sim_mouse.generation,
                   fitness=sim_mouse.fitness,
-                  genome=sim_mouse.genome)
+                  genome=sim_mouse.genome,
+                  gid=sim_mouse.gid)
 
     pygame.init()
     if best_simulation:
         pygame.display.set_caption(f"Micromouse Neuroevolution - Bestest mouse")
+    elif user_controlled:
+        pygame.display.set_caption(f"Micromouse Neuroevolution - Test")
     else:
         pygame.display.set_caption(f"Micromouse Neuroevolution - Gen {mouse.generation}")
 
@@ -61,6 +74,8 @@ def run(sim_mouse=None, maze=None, config=None):
     screen_width = maze_pixel_size + dashboard_width
 
     screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.event.pump()
+    pygame.display.flip()
     clock = pygame.time.Clock()
     maze_offset_y = (screen_height - maze_pixel_size) // 2
 
@@ -81,18 +96,22 @@ def run(sim_mouse=None, maze=None, config=None):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        if mouse.net is None:
-            mouse.net = neat.nn.FeedForwardNetwork.create(mouse.genome, config)
+            move_with_keys(event, maze, mouse)
+
+        if mouse.net is None and not user_controlled:
+            mouse.net = neat.nn.RecurrentNetwork.create(mouse.genome, config)
+
         # SIMULATION
         if mouse.alive:
             # Gestione Velocità: se premi S fai 20 step logici in un solo frame grafico
             steps_per_frame = 3 if keys[pygame.K_s] else 1
 
             for _ in range(steps_per_frame):
-                if mouse.alive:
+                if mouse.alive and not user_controlled:
                     move_with_network(maze, mouse)
                 else:
-                    break  # Se muore durante il fast-forward, ferma il loop interno
+                    break
+
         elif not best_simulation:
             running = False
 
